f49d83cbfc2016d0086b07fbcf17695b4683ce55
Sakar Aryal <techiesakar@gmail.com>
2021-09-21 20:15:11 +0545

    CSS Fixed

--- inc/enqueue-scripts.php
+++ inc/enqueue-scripts.php
@@ -1,16 +1,7 @@
 <?php
 function blogietech_scripts()
 {
-    wp_enqueue_style(
-        'blogietech-header',
-        get_template_directory_uri() . '/assets/css/header.css',
-        array(),
-        filemtime(get_template_directory() . '/assets/css/header.css'),
-        false
-    );
-    // for home only
-
-    // if (is_home()) {
+    if (!is_singular()) {
 
         wp_enqueue_style(
             'blogietech-mix-posts',
@@ -20,9 +11,7 @@ function blogietech_scripts()
             false
         );
 
-       
-    // };
-
+    };
     if (is_singular()) {
 
         wp_enqueue_style(
@@ -33,50 +22,15 @@ function blogietech_scripts()
             false
         );
     };
-    wp_enqueue_style(
-        'blogietech-gallery',
-        get_template_directory_uri() . '/assets/css/gallery.css',
-        array(),
-        filemtime(get_template_directory() . '/assets/css/gallery.css'),
-        false
-    );
-
     wp_enqueue_style('blogietech-style', get_stylesheet_uri(), [], filemtime(get_template_directory() . '/style.css'));
     wp_style_add_data('blogietech-style', 'rtl', 'replace');
-
-    wp_enqueue_style(
-        'blogietech-sidebar',
-        get_template_directory_uri() . '/assets/css/sidebar.css',
-        array(),
-        filemtime(get_template_directory() . '/assets/css/sidebar.css'),
-        false
-    );
-
-    wp_enqueue_style(
-        'blogietech-footer',
-        get_template_directory_uri() . '/assets/css/footer.css',
-        array(),
-        filemtime(get_template_directory() . '/assets/css/footer.css'),
-        false
-    );
-
-    // wp_enqueue_style('fonts-css', get_template_directory_uri() . '/assets/css/fonts.css', [], false, 'all');
-
-
-
-
     wp_enqueue_script(
-        //For Navigation
         'blogietech-navigation',
         get_template_directory_uri() . '/js/navigation.js',
         array(),
         filemtime(get_template_directory() . '/js/navigation.js'),
         true
     );
-
-
-
-
 
     if (is_singular() && comments_open() && get_option('thread_comments')) {
         wp_enqueue_script('comment-reply');
