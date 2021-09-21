26ab9df6dc4db7c679ef9dd76e4e3ac4a45b628b
Sakar Aryal <techiesakar@gmail.com>
2021-09-21 21:06:10 +0545

    CSS Formatted

--- functions.php
+++ functions.php
@@ -29,9 +29,6 @@ if (!function_exists('blogietech_setup')) :
 	{
 		/*
 		 * Make theme available for translation.
-		 * Translations can be filed in the /languages/ directory.
-		 * If you're building a theme based on Blogietech, use a find and replace
-		 * to change 'blogietech' to the name of your theme in all the template files.
 		 */
 		load_theme_textdomain('blogietech', get_template_directory() . '/languages');
 
@@ -40,22 +37,14 @@ if (!function_exists('blogietech_setup')) :
 
 		/*
 		 * Let WordPress manage the document title.
-		 * By adding theme support, we declare that this theme does not use a
-		 * hard-coded <title> tag in the document head, and expect WordPress to
-		 * provide it for us.
 		 */
 		add_theme_support('title-tag');
 
 		/*
-		 * Enable support for Post Thumbnails on posts and pages.
-		 *
-		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-		 */
+		 * Enable support for Post Thumbnails on posts and pages. */
 		add_theme_support('post-thumbnails');
 		add_image_size('mix-post-landscape', 600, 314, true);
 
-
-		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus(
 			array(
 				'primary-menu' => esc_html__('Primary', 'blogietech'),
@@ -64,10 +53,6 @@ if (!function_exists('blogietech_setup')) :
 			)
 		);
 
-		/*
-		 * Switch default core markup for search form, comment form, and comments
-		 * to output valid HTML5.
-		 */
 		add_theme_support(
 			'html5',
 			array(
@@ -81,7 +66,7 @@ if (!function_exists('blogietech_setup')) :
 			)
 		);
 
-		// Set up the WordPress core custom background feature.
+
 		add_theme_support(
 			'custom-background',
 			apply_filters(
@@ -93,14 +78,9 @@ if (!function_exists('blogietech_setup')) :
 			)
 		);
 
-		// Add theme support for selective refresh for widgets.
+
 		add_theme_support('customize-selective-refresh-widgets');
 
-		/**
-		 * Add support for core custom logo.
-		 *
-		 * @link https://codex.wordpress.org/Theme_Logo
-		 */
 		add_theme_support(
 			'custom-logo',
 			array(
@@ -128,13 +108,6 @@ function blogietech_content_width()
 	$GLOBALS['content_width'] = apply_filters('blogietech_content_width', 640);
 }
 add_action('after_setup_theme', 'blogietech_content_width', 0);
-
-/**
- * Register widget area.
- *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
- */
-
 
 /**
  * Enqueue scripts and styles.
@@ -174,23 +147,25 @@ function new_excerpt_more($more)
 }
 add_filter('excerpt_more', 'new_excerpt_more');
 
-// Filter except length to 35 words.
-// blogietech custom excerpt length
 function blogietech_excerpt_length($length)
 {
 	return 28;
 }
 add_filter('excerpt_length', 'blogietech_excerpt_length', 999);
 
+/** * Completely Remove jQuery From WordPress Admin Dashboard */
 
 
-
+if ( !is_user_logged_in() ) {
+	add_action('wp_enqueue_scripts', 'no_more_jquery');
+	function no_more_jquery(){
+		wp_deregister_script('jquery');
+	}
+ } 
+/** Plugins ** */
 require get_template_directory() . '/inc/author-social-profiles.php';
-
 require get_template_directory() . '/inc/plugins/subtitle.php';
-
 require get_template_directory() . '/inc/plugins/view-counter.php';
-
 require get_template_directory() . '/inc/plugins/emoji-off.php';
 require get_template_directory() . '/inc/plugins/enable-featured-image.php';
 require get_template_directory() . '/inc/plugins/between-related-posts.php';
